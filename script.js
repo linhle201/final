116b2327a5abee55bdcc9440d31153121f973ccc
Linh Le <lethimylinh.2001@gmail.com>
2023-12-12 11:53:22 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('show-menu');
+});
+
+// Function to handle the 'More about me' and 'Read More' buttons
+function openBox() {
+  // Add your logic here for what should happen when these buttons are clicked
+  // For example, displaying more information or redirecting to a different page
+}
